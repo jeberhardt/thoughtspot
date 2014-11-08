21324cb1a9b42beaf457befc722c8a1dcb2ed48e
Marc Pelland <mpelland@infusion.com>
2014-11-08 17:43:51 -0500

    add custom markers enhance marker interactivity appearance enhancements on maps page

--- checkitoff2/www/js/walkit.js
+++ checkitoff2/www/js/walkit.js
@@ -3,7 +3,10 @@ var walk = {
     latlon: null,
     directionsDisplay: null,
     directionsService: null,
-
+    focus: [],
+    infowindow: null,
+    markerImage: 'img/recreational_icon.png',
+    curLocMarkerImage: 'img/current_location.png',
 	initialize: function() {
 		navigator.geolocation.getCurrentPosition(walk.onMapSuccess, walk.onMapError);
 	},
@@ -16,14 +19,17 @@ var walk = {
         var opts = {
             center: walk.latlon,
             zoom: 16,
+            maxZoom: 19,
+            minZoom: 11,
             mapTypeId: google.maps.MapTypeId.ROADMAP,
-            streetViewControl: false
+            streetViewControl: false,
+            zoomControl: true,
         };
 
         walk.map = new google.maps.Map(document.getElementById("geolocation"), opts);
 
         walk.initMaps(walk.latlon);
-
+        walk.getWeather();
     },
     onMapError: function(error) {
         alert("error : " + error.code + "\nmessage: " + error.message + "\n");
@@ -35,8 +41,38 @@ var walk = {
         var marker = new google.maps.Marker({
             position: pos,
             map: walk.map,
-            title:"you be here!"
+            icon: walk.curLocMarkerImage,
+            title: "you are here"
         });
+
+        walk.attachMessage(marker, "you are here");
+
+        // google.maps.event.addListener(marker, 'click', function() {
+			// walk.infowindow.open(walk.map, marker);
+		// });
+
+    },
+    attachMessage: function(marker, message) {
+		
+		var infowindow = new google.maps.InfoWindow({
+			content: message
+		});
+
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(marker.get('map'), marker);
+		});
+	},
+
+    getWeather: function() {
+    	$.ajax({
+		  	url : "http://api.wunderground.com/api/ce47fb1f679fe7b3/geolookup/q/" + walk.latlon.lat() + "," + walk.latlon.lng() + ".json",
+		  	dataType : "jsonp",
+		  	success : function(parsed_json) {
+		  		// var location = parsed_json['location']['city'];
+		  		// var temp_f = parsed_json['current_observation']['temp_f'];
+		  		// alert("Current temperature in " + location + " is: " + temp_f);
+		  	}
+		});
     },
     onMapDataLoaded: function() {
         var localStuff = dataModel.getWithinDistance(dataModel.data, 1, walk.latlon.lat(), walk.latlon.lng());
@@ -55,14 +91,23 @@ var walk = {
 
             var marker = new google.maps.Marker({
                 position: latlon,
-                map: walk.map
-                // title:"you be here!"
+                map: walk.map,
+                icon: walk.markerImage,
+                title: locations[i]["INCIDENT TITLE"]
             });
 
-            var div = "<div class='location-list-item'>" + locations[i]["INCIDENT TITLE"] + "</div>";
+            walk.attachMessage(marker, locations[i]["INCIDENT TITLE"]);
+
+            var div = "<div class='location-list-item'><p>" + locations[i]["INCIDENT TITLE"] + "<br>" + locations[i]["Address 1"] + "</p></div>";
             $("#mapListing").append($(div));
+            
 
         }
+
+        $(".location-list-item").dotdotdot({
+				//	configuration goes here
+			});
+
         var loc1 = locations[Math.floor(Math.random() * (locations.length -1))];
         var loc2 = locations[Math.floor(Math.random() * (locations.length -1))];
 
@@ -74,7 +119,8 @@ var walk = {
 
     generateWalkRoute: function(start, pts) {
         walk.directionsService = new google.maps.DirectionsService();
-        walk.directionsDisplay = new google.maps.DirectionsRenderer();
+        walk.directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true});
+		
         walk.directionsDisplay.setMap(walk.map);
 
         walk.calcRoute(start, pts);
