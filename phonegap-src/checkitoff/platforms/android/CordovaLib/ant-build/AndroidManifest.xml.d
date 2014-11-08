fe2aeada3537e9780683d6cc36185d47372cc203
Marc Pelland <mpelland@infusion.com>
2014-11-08 09:29:50 -0500

    check in before james

--- phonegap-src/checkitoff/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ phonegap-src/checkitoff/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/mpelland/Documents/working/CC/HackAThought/checkitoff/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/mpelland/Documents/working/CC/HackAThought/checkitoff/platforms/android/CordovaLib/AndroidManifest.xml \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/CordovaLib/AndroidManifest.xml \
--- phonegap-src/checkitoff/platforms/android/ant-build/classes.dex.d
+++ phonegap-src/checkitoff/platforms/android/ant-build/classes.dex.d
@@ -1,9 +1,13 @@
-/Users/mpelland/Documents/working/CC/HackAThought/checkitoff/platforms/android/ant-build/classes.dex : \
-/Users/mpelland/Documents/working/CC/HackAThought/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/BuildConfig.class \
-/Users/mpelland/Documents/working/CC/HackAThought/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/checkitoff.class \
-/Users/mpelland/Documents/working/CC/HackAThought/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/R$attr.class \
-/Users/mpelland/Documents/working/CC/HackAThought/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/R$drawable.class \
-/Users/mpelland/Documents/working/CC/HackAThought/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/R$string.class \
-/Users/mpelland/Documents/working/CC/HackAThought/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/R$xml.class \
-/Users/mpelland/Documents/working/CC/HackAThought/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/R.class \
-/Users/mpelland/Documents/working/CC/HackAThought/checkitoff/platforms/android/ant-build/dexedLibs/classes-41cdafae683b44e8aca5f31273ee84c6.jar \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/classes.dex : \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/BuildConfig.class \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/checkitoff.class \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/R$attr.class \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/R$drawable.class \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/R$string.class \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/R$xml.class \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/classes/com/cttoronto/hackathought/checkitoff/R.class \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/classes/org/apache/cordova/devicemotion/AccelListener$1.class \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/classes/org/apache/cordova/devicemotion/AccelListener.class \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/classes/org/apache/cordova/deviceorientation/CompassListener$1.class \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/classes/org/apache/cordova/deviceorientation/CompassListener.class \
+/Users/mpelland/Documents/working/CC/HackAThought/repo/phonegap-src/checkitoff/platforms/android/ant-build/dexedLibs/classes-60f327b7ba6d4a117a6f9b8f99232f22.jar \
