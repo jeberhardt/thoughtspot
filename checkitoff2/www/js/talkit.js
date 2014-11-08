ee64ba033c48798ef45aa7bf9d734c9edd5e055b
jameseberhardt <jeberhardt@insightactioninc.com>
2014-11-08 18:33:23 -0500

    more code for the "talk it" section.

--- checkitoff2/www/js/talkit.js
+++ checkitoff2/www/js/talkit.js
@@ -16,40 +16,57 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    
+var talkitPage = {
     initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-
         $("#contact-link").on("tap", function(e){
             e.preventDefault();
-        });       
-        
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        // var parentElement = document.getElementById(id);
-        // var listeningElement = parentElement.querySelector('.listening');
-        // var receivedElement = parentElement.querySelector('.received');
-
-        // listeningElement.setAttribute('style', 'display:none;');
-        // receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    },
+            console.log("Whatever");
+            $("body").addClass('ui-disabled').css("background", "#000");
+            $.mobile.loading("show");
+            var options = new ContactFindOptions();
+            options.filter = "";
+            options.multiple = true;
+            var filter = ["displayName", "phoneNumbers"];
+            navigator.contacts.find(filter, onSuccess, onError, options);
+        });
+    }
 };
+
+function onSuccess(contacts) {
+    console.log("onSuccess");
+    var html = "";
+    for (var i = 0; i < contacts.length; i++) {
+        console.log(contacts[i]);
+        if ($.trim(contacts[i].displayName).length != 0 || $.trim(contacts[i].nickName).length != 0 || $.trim(contacts[i].name.formatted).length !=0) {
+            html += '<li>';
+            html += '<h2>' + contacts[i].displayName ? contacts[i].displayName : contacts[i].name.formatted + '</h2>';
+            if (contacts[i].phoneNumbers) {
+                html += '<ul class="innerlsv" data-role="listview" data-inset="true">';
+                html += '<li><h3>Phone Numbers</h3></li>';
+                for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+                    html += "<li>Type: " + contacts[i].phoneNumbers[j].type + "<br/>" +
+                        "Value: " + contacts[i].phoneNumbers[j].value + "<br/>" +
+                        "Preferred: " + contacts[i].phoneNumbers[j].pref + "</li>";
+                }
+                html += "</ul>";
+            }
+            html += '</li>';
+        }
+    }
+    console.log(html);
+    if (contacts.length === 0) {
+        html = '<li>';
+        html += '<h2>No Contacts</h2>';
+        html += '</li>';
+    }
+    $("#contactsList").html(html);
+    $("#contactsList").listview().listview('refresh');
+    $(".innerlsv").listview().listview('refresh');
+    $("body").removeClass('ui-disabled');
+}
+
+function onError(contactError) {
+    alert('Oops Something went wrong!');
+    $.mobile.loading("hide");
+    $("body").removeClass('ui-disabled');
+}
